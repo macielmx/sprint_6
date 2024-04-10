a8615cba6e47603cbb27869e1af81fb6f47fcbc3
macielmx <macielmx@gmail.com>
2024-04-10 16:34:41 -0600

    Actualización de la app web

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Herramienta para la elección de autos usados') #crear de un encabezado
+
+car_data = pd.read_csv('/Users/lmaciel/Mis_Proyectos/sprint_6/vehicles_us.csv') # leer los datos
+
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+    st.write('Creando histograma de los autos anunciados para su venta') # escribir un mensaje     
+    fig = px.histogram(car_data, x="odometer") # crear un histograma      
+    st.plotly_chart(fig, use_container_width=True) # mostrar un gráfico Plotly interactivo
+
+scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
+        
+if scatter_button: # al hacer clic en el botón
+    st.write('Creando gráfico de despersión de los autos anunciados para su venta') # escribir un mensaje     
+    fig = px.scatter(car_data, x="odometer") # crear un histograma      
+    st.plotly_chart(fig, use_container_width=True) # mostrar un gráfico Plotly interactivo
